a6882a8a3cb321928eddbfc83cd9d9e19de5b828
Cristiano Castro <cristianodmtsb@gmail.com>
2024-09-12 18:30:06 -0300

    ajuste no famulario e configuração de submit

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 import { generateReturnsArray } from './src/investmentGoals';
 
-const calculateButton = document.getElementById('btn-calcular-investimento');
+const formInvestimento = document.getElementById('formulario-investimento');
+const btnCalcularInvestimento = document.getElementById(
+  'btn-calcular-investimento',
+);
 
-function renderInvestments() {
-  const aporteInicial = Number(document.getElementById('aporte-inicial').value);
-  const aporteMensal = Number(document.getElementById('aporte-mensal').value);
+function renderInvestments(e) {
+  e.preventDefault();
+  const aporteInicial = Number(formInvestimento['aporte-inicial'].value);
+  const aporteMensal = Number(formInvestimento['aporte-mensal'].value);
   const tempodeContribuicao = Number(
-    document.getElementById('prazo-de-aportes').value,
+    formInvestimento['prazo-de-aportes'].value,
   );
-  const periodoContribuicao = document.getElementById(
-    'periodo-prazo-de-aportes',
-  ).value;
-  const rentabilidade = Number(document.getElementById('rentabilidade').value);
-  const periodoRentabilidade = document.getElementById(
-    'periodo-rentabilidade',
-  ).value;
-  const taxaSobreLucro = Number(
-    document.getElementById('taxas-sobre-lucro').value,
-  );
+  const periodoContribuicao =
+    formInvestimento['periodo-prazo-de-aportes'].value;
+  const rentabilidade = Number(formInvestimento['rentabilidade'].value);
+  const periodoRentabilidade = formInvestimento['periodo-rentabilidade'].value;
+  const taxaSobreLucro = Number(formInvestimento['taxas-sobre-lucro'].value);
 
   const returnsArray = generateReturnsArray(
     aporteInicial,
@@ -31,4 +30,5 @@ function renderInvestments() {
   console.log(returnsArray);
 }
 
-calculateButton.addEventListener('click', renderInvestments);
+// formInvestimento.addEventListener('submit', renderInvestments);
+btnCalcularInvestimento.addEventListener('click', renderInvestments);
